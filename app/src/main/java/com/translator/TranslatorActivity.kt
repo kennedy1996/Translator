1fca993624613184a573cfa814d6f23ab934069f
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-10-23 18:37:16 +0100

    Initial commit

---
+++ app/src/main/java/com/translator/TranslatorActivity.kt
@@ -0,0 +1,11 @@
+package com.translator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TranslatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.translator_activity)
+    }
+}
